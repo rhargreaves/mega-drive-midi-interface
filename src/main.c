de0732e371a984031710221aed37eb44e9b16269
rhargreaves <robert@roberthargreaves.net>
2018-11-03 13:20:56 +0000

    Add header

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <genesis.h>
 
+static const u16 MAX_X = 40;
+static const char HEADER[] = "Sega Mega Drive MIDI Interface";
+static const char BUILD[] = "Build Master";
+
 int main(void)
 {
+    while (TRUE) {
+        VDP_showFPS(FALSE);
+        VDP_drawText(HEADER, (MAX_X - sizeof(HEADER)) / 2, 2);
+        VDP_drawText(BUILD, (MAX_X - sizeof(BUILD)) / 2, 3);
+        VDP_waitVSync();
+    }
 }
