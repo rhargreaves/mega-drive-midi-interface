37a6bc81d49986dcd888bd6e5863efe84ef64a5a
rhargreaves <robert@roberthargreaves.net>
2018-11-04 15:37:10 +0000

    Remove fluff

--- src/main.c
+++ src/main.c
@@ -6,21 +6,13 @@
 static const u16 MAX_X = 40;
 static const char HEADER[] = "Sega Mega Drive MIDI Interface";
 
-static void vsync(void);
-
 int main(void)
 {
-    synth_init();
-    SYS_setVIntCallback(vsync);
     VDP_drawText(HEADER, (MAX_X - sizeof(HEADER)) / 2, 2);
     VDP_drawText(BUILD, (MAX_X - sizeof(BUILD)) / 2, 3);
+    interface_init();
     while (TRUE) {
         interface_tick();
         VDP_waitVSync();
     }
-}
-
-static void vsync(void)
-{
-    VDP_showFPS(FALSE);
 }
