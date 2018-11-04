4d1c9405c9b3e078c103b7e77a536056ab324da0
rhargreaves <robert@roberthargreaves.net>
2018-11-04 16:16:59 +0000

    Separate midi tests

--- tests/test_main.c
+++ tests/test_main.c
@@ -3,22 +3,9 @@
 #include <stddef.h>
 
 #include <cmocka.h>
-#include <comm.h>
-#include <interface.h>
-#include <midi.h>
-#include <wraps.h>
-
 #include <test_interface.c>
+#include <test_midi.c>
 #include <test_synth.c>
-
-static void midi_triggers_synth_note_on(void** state)
-{
-    expect_value(__wrap_synth_noteOn, channel, 0);
-
-    Message noteOn = { 0b10010000, 0x40, 127 };
-
-    __real_midi_process(&noteOn);
-}
 
 int main(void)
 {
---
+++ tests/test_midi.c
@@ -0,0 +1,15 @@
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+#include <cmocka.h>
+#include <midi.h>
+
+static void midi_triggers_synth_note_on(void** state)
+{
+    expect_value(__wrap_synth_noteOn, channel, 0);
+
+    Message noteOn = { 0b10010000, 0x40, 127 };
+
+    __real_midi_process(&noteOn);
+}
