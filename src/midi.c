25b68a356d6c4187c2f665e376f1324701f81f72
rhargreaves <robert@roberthargreaves.net>
2018-11-05 21:22:37 +0000

    Define magic numbers

--- src/midi.c
+++ src/midi.c
@@ -4,6 +4,23 @@
 static u8 midi_getOctave(u8 pitch);
 static u16 midi_getFreqNumber(u8 pitch);
 
+static const u8 MIN_MIDI_PITCH = 23;
+static const u8 SEMITONES = 12;
+static const u16 FREQ_NUMBERS[] = {
+    617, // B
+    653,
+    692,
+    733,
+    777,
+    823,
+    872,
+    924,
+    979,
+    1037,
+    1099,
+    1164 // A#
+};
+
 void midi_process(Message* message)
 {
     if ((message->status & 0b10010000) == 0b10010000) {
@@ -18,24 +35,11 @@ void midi_process(Message* message)
 
 static u8 midi_getOctave(u8 pitch)
 {
-    return ((pitch - 23) / 12);
+    return (pitch - MIN_MIDI_PITCH) / SEMITONES;
 }
 
 static u16 midi_getFreqNumber(u8 pitch)
 {
-    const u16 freqNumbers[] = {
-        617, // B
-        653,
-        692,
-        733,
-        777,
-        823,
-        872,
-        924,
-        979,
-        1037,
-        1099,
-        1164 // A#
-    };
-    return freqNumbers[(u8)(pitch - 23) % 12];
+
+    return FREQ_NUMBERS[((u8)(pitch - MIN_MIDI_PITCH)) % SEMITONES];
 }
