10be29d3ddc7757e80725dfa047ad9da195a94c4
rhargreaves <robert@roberthargreaves.net>
2018-11-05 00:23:55 +0000

    Use GCC mod

--- src/midi.c
+++ src/midi.c
@@ -3,7 +3,6 @@
 
 static u8 midi_getOctave(u8 pitch);
 static u16 midi_getFreqNumber(u8 pitch);
-static u16 midi_modulo(u16 a, u16 b);
 
 void midi_process(Message* message)
 {
@@ -38,14 +37,5 @@ static u16 midi_getFreqNumber(u8 pitch)
         1099,
         1164 // A#
     };
-    return freqNumbers[midi_modulo(pitch - 23, 12)];
-}
-
-static u16 midi_modulo(u16 a, u16 b)
-{
-    u16 c = a - b;
-    while (c >= b) {
-        c = c - b;
-    }
-    return c;
+    return freqNumbers[(u8)(pitch - 23) % 12];
 }
