e52f7355199200e77c3d477f92d9338b9e212f7b
rhargreaves <robert@roberthargreaves.net>
2018-11-10 18:09:07 +0000

    Refactor

--- src/interface.c
+++ src/interface.c
@@ -6,6 +6,9 @@
 
 static u8 lastUnknownStatus = 0;
 
+static void noteOn(u8 status);
+static void noteOff(u8 status);
+
 void interface_init(void)
 {
     synth_init();
@@ -23,21 +26,31 @@ void interface_tick(void)
     u8 status = comm_read();
     u8 upperStatus = status & 0xF0;
     if (upperStatus == 0x90) {
-        u8 chan = status & 0x0F;
-        u8 pitch = comm_read();
-        u8 velocity = comm_read();
-        midi_noteOn(
-            chan,
-            pitch,
-            velocity);
+        noteOn(status);
     } else if (upperStatus == 0x80) {
-        u8 chan = status & 0x0F;
-        comm_read();
-        comm_read();
-        midi_noteOff(chan);
+        noteOff(status);
     } else {
         lastUnknownStatus = status;
     }
+}
+
+static void noteOn(u8 status)
+{
+    u8 chan = status & 0x0F;
+    u8 pitch = comm_read();
+    u8 velocity = comm_read();
+    midi_noteOn(
+        chan,
+        pitch,
+        velocity);
+}
+
+static void noteOff(u8 status)
+{
+    u8 chan = status & 0x0F;
+    comm_read();
+    comm_read();
+    midi_noteOff(chan);
 }
 
 u8 interface_lastUnknownStatus(void)
