79b055ed2fffefa88e93ea9ce7895902744399a7
rhargreaves <robert@roberthargreaves.net>
2018-11-11 20:08:28 +0000

    Refactor

--- src/psg_chip.c
+++ src/psg_chip.c
@@ -2,21 +2,36 @@
 #include <psg.h>
 #include <types.h>
 
-static u8 attenuations[] = { 0, 0, 0, 0 };
+#define ATTENUATION_OFF 0xF
+#define ATTENUATION_ON 0
+
+static void applyAttenuation(u8 channel);
+
+static u8 attenuations[] = {
+    ATTENUATION_ON,
+    ATTENUATION_ON,
+    ATTENUATION_ON,
+    ATTENUATION_ON
+};
+
+static void applyAttenuation(u8 channel)
+{
+    PSG_setEnvelope(channel, attenuations[channel]);
+}
 
 void psg_noteOff(u8 channel)
 {
-    PSG_setEnvelope(channel, 0xF);
+    PSG_setEnvelope(channel, ATTENUATION_OFF);
 }
 
 void psg_noteOn(u8 channel, u16 freq)
 {
     PSG_setFrequency(channel, freq);
-    PSG_setEnvelope(channel, attenuations[channel]);
+    applyAttenuation(channel);
 }
 
 void psg_attenuation(u8 channel, u8 attenuation)
 {
     attenuations[channel] = attenuation;
-    PSG_setEnvelope(channel, attenuations[channel]);
+    applyAttenuation(channel);
 }
