187288f1a5f68ca8934cad280cbd99fd5ee9d3f6
rhargreaves <robert@roberthargreaves.net>
2018-11-11 00:44:56 +0000

    Refactor

--- src/interface.c
+++ src/interface.c
@@ -4,9 +4,6 @@
 #include <string.h>
 #include <synth.h>
 
-static u8 lastUnknownStatus = 0;
-static ControlChange lastUnknownControlChange;
-
 #define STATUS_CHANNEL(status) status & 0x0F
 #define STATUS_EVENT(status) status >> 4
 
@@ -16,6 +13,9 @@ static ControlChange lastUnknownControlChange;
 
 #define CC_VOLUME 0x7
 #define CC_PAN 0xA
+
+static u8 lastUnknownStatus = 0;
+static ControlChange lastUnknownControlChange;
 
 static void noteOn(u8 status);
 static void noteOff(u8 status);
@@ -37,14 +37,19 @@ void interface_tick(void)
 {
     u8 status = comm_read();
     u8 event = STATUS_EVENT(status);
-    if (event == EVENT_NOTE_ON) {
+    switch (event) {
+    case EVENT_NOTE_ON:
         noteOn(status);
-    } else if (event == EVENT_NODE_OFF) {
+        break;
+    case EVENT_NODE_OFF:
         noteOff(status);
-    } else if (event == EVENT_CC) {
+        break;
+    case EVENT_CC:
         controlChange(status);
-    } else {
+        break;
+    default:
         lastUnknownStatus = status;
+        break;
     }
 }
 
@@ -58,13 +63,17 @@ static void controlChange(u8 status)
     u8 chan = STATUS_CHANNEL(status);
     u8 controller = comm_read();
     u8 value = comm_read();
-    if (controller == CC_VOLUME) {
+    switch (controller) {
+    case CC_VOLUME:
         midi_channelVolume(chan, value);
-    } else if (controller == CC_PAN) {
+        break;
+    case CC_PAN:
         midi_pan(chan, value);
-    } else {
+        break;
+    default:
         lastUnknownControlChange.controller = controller;
         lastUnknownControlChange.value = value;
+        break;
     }
 }
 
