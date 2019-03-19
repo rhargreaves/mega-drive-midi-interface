d116490f82e73a9ed60ac26a518604a6bdacc181
rhargreaves <robert@roberthargreaves.net>
2019-03-19 23:57:58 +0000

    Position Sonic away from edge of screen

--- src/sonic.c
+++ src/sonic.c
@@ -46,7 +46,7 @@ void sonic_init(void)
     SYS_disableInts();
     SPR_init(16, 256, 256);
     sprite = SPR_addSprite(&sonic_sprite, fix32ToInt(FIX32(0)),
-        fix32ToInt(FIX32(0)), TILE_ATTR(PAL3, TRUE, FALSE, FALSE));
+        fix32ToInt(FIX32(8)), TILE_ATTR(PAL3, TRUE, FALSE, FALSE));
     SPR_setAnim(sprite, (s16)currentState);
     SPR_update();
     VDP_setPaletteColors(
