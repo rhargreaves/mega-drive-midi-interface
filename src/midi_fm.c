c2823eb85d0c4355326667b693700be58902367f
rhargreaves <robert@roberthargreaves.net>
2019-09-05 19:37:49 +0100

    Refactor

--- src/midi_fm.c
+++ src/midi_fm.c
@@ -2,6 +2,7 @@
 #include "synth.h"
 
 static const u8 MIN_MIDI_PITCH = 11;
+static const u8 MAX_MIDI_PITCH = 106;
 static const u8 SEMITONES = 12;
 static const u16 FREQ_NUMBERS[] = {
     617, // B
@@ -19,12 +20,13 @@ static const u8 TOTAL_LEVELS[] = { 127, 122, 117, 113, 108, 104, 100, 97, 93,
 
 static u8 octave(u8 pitch);
 static u16 freqNumber(u8 pitch);
+static u8 pitch_is_out_of_range(u8 pitch);
 
 static u8 pitches[MAX_FM_CHANS];
 
 void midi_fm_noteOn(u8 chan, u8 pitch, u8 velocity)
 {
-    if (pitch < 11 || pitch > 106) {
+    if (pitch_is_out_of_range(pitch)) {
         return;
     }
     pitches[chan] = pitch;
@@ -64,4 +66,9 @@ static u8 octave(u8 pitch)
 static u16 freqNumber(u8 pitch)
 {
     return FREQ_NUMBERS[((u8)(pitch - MIN_MIDI_PITCH)) % SEMITONES];
+}
+
+static u8 pitch_is_out_of_range(u8 pitch)
+{
+    return pitch < MIN_MIDI_PITCH || pitch > MAX_MIDI_PITCH;
 }
