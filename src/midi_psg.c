72277e996656c57c0b720915ea1b1f0a88f8ebf0
rhargreaves <robert@roberthargreaves.net>
2019-09-08 17:56:34 +0100

    Refactor

--- src/midi_psg.c
+++ src/midi_psg.c
@@ -4,12 +4,9 @@
 
 #define MIN_PSG_CHAN 6
 #define MAX_PSG_CHAN 9
+#define MIN_MIDI_KEY 45
 
-static u8 psgChannel(u8 midiChannel);
-
-static const u16 FREQUENCIES[] = { 8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15,
-    16, 17, 18, 19, 21, 22, 23, 25, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46,
-    49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 93, 98, 104, 110, 117, 123, 131,
+static const u16 FREQUENCIES[] = { 110, 117, 123, 131,
     139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311,
     330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740,
     784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568,
@@ -30,19 +27,17 @@ static u8 attenuations[] = { PSG_ATTENUATION_LOUDEST, PSG_ATTENUATION_LOUDEST,
     PSG_ATTENUATION_LOUDEST, PSG_ATTENUATION_LOUDEST };
 static bool notesOn[MAX_PSG_CHANS];
 
-static u8 psgChannel(u8 midiChannel)
-{
-    return midiChannel - MIN_PSG_CHAN;
-}
+static u8 psgChannel(u8 midiChannel);
+static u16 freqForMidiKey(u8 midiKey);
 
-void midi_psg_noteOn(u8 chan, u8 pitch, u8 velocity)
+void midi_psg_noteOn(u8 chan, u8 key, u8 velocity)
 {
-    if(pitch < 45) {
+    if(key < MIN_MIDI_KEY) {
         return;
     }
     u8 psgChan = psgChannel(chan);
-    pitches[psgChan] = pitch;
-    psg_frequency(psgChan, FREQUENCIES[pitch]);
+    pitches[psgChan] = key;
+    psg_frequency(psgChan, freqForMidiKey(key));
     psg_attenuation(psgChan, attenuations[psgChan]);
     notesOn[psgChan] = true;
 }
@@ -67,7 +62,7 @@ void midi_psg_pitchBend(u8 chan, u16 bend)
 {
     u8 psgChan = psgChannel(chan);
     u8 origPitch = pitches[psgChan];
-    u16 freq = FREQUENCIES[origPitch];
+    u16 freq = freqForMidiKey(origPitch);
 
     s16 bendRelative = bend - 0x2000;
     freq = freq + (bendRelative / 100);
@@ -86,4 +81,14 @@ void midi_psg_reset(void)
         notesOn[chan] = false;
         attenuations[chan] = PSG_ATTENUATION_LOUDEST;
     }
+}
+
+static u8 psgChannel(u8 midiChannel)
+{
+    return midiChannel - MIN_PSG_CHAN;
+}
+
+static u16 freqForMidiKey(u8 midiKey)
+{
+    return FREQUENCIES[midiKey-MIN_MIDI_KEY];
 }
