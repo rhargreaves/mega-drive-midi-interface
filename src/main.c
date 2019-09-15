3fa331034138647e1a43dca3c010fb1743ebc885
rhargreaves <robert@roberthargreaves.net>
2019-09-16 00:29:38 +0100

    Refactor

--- src/main.c
+++ src/main.c
@@ -4,16 +4,22 @@
 #include "sys.h"
 #include "ui.h"
 
+static void initAllModules(void);
 static void vsync(void);
 
 int main(void)
+{
+    initAllModules();
+    SYS_setVIntCallback(vsync);
+    midi_receiver_perpectual_read();
+}
+
+static void initAllModules(void)
 {
     synth_init();
     ui_init();
     midi_init();
     midi_receiver_init();
-    SYS_setVIntCallback(vsync);
-    midi_receiver_perpectual_read();
 }
 
 static void vsync(void)
