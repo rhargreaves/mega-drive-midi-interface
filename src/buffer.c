3bd9e6d87bb17ba2f1516b6fb1894c87a242899f
rhargreaves <robert@roberthargreaves.net>
2019-11-18 18:50:51 +0000

    Writes to buffer are by ISR so buffer needs to be set as volatile

--- src/buffer.c
+++ src/buffer.c
@@ -1,16 +1,15 @@
 #include <genesis.h>
 
-#define BUFFER_LEN 512
+#define BUFFER_SIZE 256
 
 static u16 readHead = 0;
-static u16 writeHead = 0;
-
-static char buffer[BUFFER_LEN];
+static volatile u16 writeHead = 0;
+static volatile char buffer[BUFFER_SIZE];
 
 u8 buffer_read(void)
 {
     u8 data = buffer[readHead++];
-    if (readHead == BUFFER_LEN) {
+    if (readHead == BUFFER_SIZE) {
         readHead = 0;
     }
     return data;
@@ -19,7 +18,7 @@ u8 buffer_read(void)
 void buffer_write(u8 data)
 {
     buffer[writeHead++] = data;
-    if (writeHead == BUFFER_LEN) {
+    if (writeHead == BUFFER_SIZE) {
         writeHead = 0;
     }
 }
@@ -39,7 +38,7 @@ u16 buffer_available(void)
         xxxxxxxxx
     */
     if (writeHead >= readHead) {
-        return BUFFER_LEN - (writeHead - readHead);
+        return BUFFER_SIZE - (writeHead - readHead);
     } else {
         return readHead - writeHead;
     }
