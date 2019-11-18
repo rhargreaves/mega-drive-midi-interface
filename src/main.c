23430dca50c607b54f3637869e3e6d3eab53fbf8
rhargreaves <robert@roberthargreaves.net>
2019-11-18 19:32:29 +0000

    Refactor

--- src/main.c
+++ src/main.c
@@ -5,25 +5,12 @@
 #include "sys.h"
 #include "ui.h"
 
-static void initAllModules(void);
-static void vsync(void);
-
-int main(void)
-{
-    initAllModules();
-    SYS_setVIntCallback(vsync);
-    midi_receiver_perpectual_read();
-}
-
-static void initAllModules(void)
+int main()
 {
     comm_init();
     midi_init((Channel**)M_BANK_0, (PercussionPreset**)P_BANK_0);
     midi_receiver_init();
     ui_init();
-}
-
-static void vsync(void)
-{
-    ui_vsync();
+    SYS_setVIntCallback(ui_vsync);
+    midi_receiver_perpectual_read();
 }
