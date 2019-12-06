474fbcac4716a3efda15095a3031e8e5c883c1b5
rhargreaves <robert@roberthargreaves.net>
2019-12-06 22:34:06 +0000

    Test all FM channels when setting GenMDM parameters

--- tests/unit/test_midi_fm.c
+++ tests/unit/test_midi_fm.c
@@ -112,76 +112,92 @@ static void test_midi_channel_volume_sets_volume(UNUSED void** state)
 
 static void test_midi_pan_sets_synth_stereo_mode_right(UNUSED void** state)
 {
-    expect_value(__wrap_synth_stereo, channel, 0);
-    expect_value(__wrap_synth_stereo, mode, 1);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_stereo, channel, chan);
+        expect_value(__wrap_synth_stereo, mode, 1);
 
-    __real_midi_cc(0, CC_PAN, 127);
+        __real_midi_cc(chan, CC_PAN, 127);
+    }
 }
 
 static void test_midi_pan_sets_synth_stereo_mode_left(UNUSED void** state)
 {
-    expect_value(__wrap_synth_stereo, channel, 0);
-    expect_value(__wrap_synth_stereo, mode, 2);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_stereo, channel, chan);
+        expect_value(__wrap_synth_stereo, mode, 2);
 
-    __real_midi_cc(0, CC_PAN, 0);
+        __real_midi_cc(chan, CC_PAN, 0);
+    }
 }
 
 static void test_midi_pan_sets_synth_stereo_mode_centre(UNUSED void** state)
 {
-    expect_value(__wrap_synth_stereo, channel, 0);
-    expect_value(__wrap_synth_stereo, mode, 3);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_stereo, channel, chan);
+        expect_value(__wrap_synth_stereo, mode, 3);
 
-    __real_midi_cc(0, CC_PAN, 32);
+        __real_midi_cc(chan, CC_PAN, 32);
 
-    expect_value(__wrap_synth_stereo, channel, 0);
-    expect_value(__wrap_synth_stereo, mode, 3);
+        expect_value(__wrap_synth_stereo, channel, chan);
+        expect_value(__wrap_synth_stereo, mode, 3);
 
-    __real_midi_cc(0, CC_PAN, 95);
+        __real_midi_cc(chan, CC_PAN, 95);
+    }
 }
 
 static void test_midi_sets_fm_algorithm(UNUSED void** state)
 {
-    expect_value(__wrap_synth_algorithm, channel, 0);
-    expect_value(__wrap_synth_algorithm, algorithm, 1);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_algorithm, channel, chan);
+        expect_value(__wrap_synth_algorithm, algorithm, 1);
 
-    __real_midi_cc(0, CC_GENMDM_FM_ALGORITHM, 20);
+        __real_midi_cc(chan, CC_GENMDM_FM_ALGORITHM, 20);
+    }
 }
 
 static void test_midi_sets_fm_feedback(UNUSED void** state)
 {
-    expect_value(__wrap_synth_feedback, channel, 0);
-    expect_value(__wrap_synth_feedback, feedback, 2);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_feedback, channel, chan);
+        expect_value(__wrap_synth_feedback, feedback, 2);
 
-    __real_midi_cc(0, CC_GENMDM_FM_FEEDBACK, 33);
+        __real_midi_cc(chan, CC_GENMDM_FM_FEEDBACK, 33);
+    }
 }
 
 static void test_midi_sets_channel_AMS(UNUSED void** state)
 {
-    expect_value(__wrap_synth_ams, channel, 0);
-    expect_value(__wrap_synth_ams, ams, 1);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_ams, channel, chan);
+        expect_value(__wrap_synth_ams, ams, 1);
 
-    __real_midi_cc(0, 76, 32);
+        __real_midi_cc(chan, 76, 32);
+    }
 }
 
 static void test_midi_sets_channel_FMS(UNUSED void** state)
 {
-    expect_value(__wrap_synth_fms, channel, 0);
-    expect_value(__wrap_synth_fms, fms, 2);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_fms, channel, chan);
+        expect_value(__wrap_synth_fms, fms, 2);
 
-    __real_midi_cc(0, 75, 32);
+        __real_midi_cc(chan, 75, 32);
+    }
 }
 
 static void test_midi_sets_operator_total_level(UNUSED void** state)
 {
     const u8 expectedValue = 50;
-    for (u8 cc = 16; cc <= 19; cc++) {
-        u8 expectedOp = cc - 16;
-        expect_value(__wrap_synth_operatorTotalLevel, channel, 0);
-        expect_value(__wrap_synth_operatorTotalLevel, op, expectedOp);
-        expect_value(
-            __wrap_synth_operatorTotalLevel, totalLevel, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 16; cc <= 19; cc++) {
+            u8 expectedOp = cc - 16;
+            expect_value(__wrap_synth_operatorTotalLevel, channel, chan);
+            expect_value(__wrap_synth_operatorTotalLevel, op, expectedOp);
+            expect_value(
+                __wrap_synth_operatorTotalLevel, totalLevel, expectedValue);
 
-        __real_midi_cc(0, cc, expectedValue);
+            __real_midi_cc(chan, cc, expectedValue);
+        }
     }
 }
 
@@ -189,13 +205,16 @@ static void test_midi_sets_operator_multiple(UNUSED void** state)
 {
     const u8 expectedValue = 4;
 
-    for (u8 cc = 20; cc <= 23; cc++) {
-        u8 expectedOp = cc - 20;
-        expect_value(__wrap_synth_operatorMultiple, channel, 0);
-        expect_value(__wrap_synth_operatorMultiple, op, expectedOp);
-        expect_value(__wrap_synth_operatorMultiple, multiple, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 20; cc <= 23; cc++) {
+            u8 expectedOp = cc - 20;
+            expect_value(__wrap_synth_operatorMultiple, channel, chan);
+            expect_value(__wrap_synth_operatorMultiple, op, expectedOp);
+            expect_value(
+                __wrap_synth_operatorMultiple, multiple, expectedValue);
 
-        __real_midi_cc(0, cc, 32);
+            __real_midi_cc(chan, cc, 32);
+        }
     }
 }
 
@@ -203,13 +222,15 @@ static void test_midi_sets_operator_detune(UNUSED void** state)
 {
     const u8 expectedValue = 2;
 
-    for (u8 cc = 24; cc <= 27; cc++) {
-        u8 expectedOp = cc - 24;
-        expect_value(__wrap_synth_operatorDetune, channel, 0);
-        expect_value(__wrap_synth_operatorDetune, op, expectedOp);
-        expect_value(__wrap_synth_operatorDetune, detune, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 24; cc <= 27; cc++) {
+            u8 expectedOp = cc - 24;
+            expect_value(__wrap_synth_operatorDetune, channel, chan);
+            expect_value(__wrap_synth_operatorDetune, op, expectedOp);
+            expect_value(__wrap_synth_operatorDetune, detune, expectedValue);
 
-        __real_midi_cc(0, cc, 32);
+            __real_midi_cc(chan, cc, 32);
+        }
     }
 }
 
@@ -217,14 +238,16 @@ static void test_midi_sets_operator_rate_scaling(UNUSED void** state)
 {
     const u8 expectedValue = 2;
 
-    for (u8 cc = 39; cc <= 42; cc++) {
-        u8 expectedOp = cc - 39;
-        expect_value(__wrap_synth_operatorRateScaling, channel, 0);
-        expect_value(__wrap_synth_operatorRateScaling, op, expectedOp);
-        expect_value(
-            __wrap_synth_operatorRateScaling, rateScaling, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 39; cc <= 42; cc++) {
+            u8 expectedOp = cc - 39;
+            expect_value(__wrap_synth_operatorRateScaling, channel, chan);
+            expect_value(__wrap_synth_operatorRateScaling, op, expectedOp);
+            expect_value(
+                __wrap_synth_operatorRateScaling, rateScaling, expectedValue);
 
-        __real_midi_cc(0, cc, 64);
+            __real_midi_cc(chan, cc, 64);
+        }
     }
 }
 
@@ -232,29 +255,32 @@ static void test_midi_sets_operator_attack_rate(UNUSED void** state)
 {
     const u8 expectedValue = 2;
 
-    for (u8 cc = 43; cc <= 46; cc++) {
-        u8 expectedOp = cc - 43;
-        expect_value(__wrap_synth_operatorAttackRate, channel, 0);
-        expect_value(__wrap_synth_operatorAttackRate, op, expectedOp);
-        expect_value(
-            __wrap_synth_operatorAttackRate, attackRate, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 43; cc <= 46; cc++) {
+            u8 expectedOp = cc - 43;
+            expect_value(__wrap_synth_operatorAttackRate, channel, chan);
+            expect_value(__wrap_synth_operatorAttackRate, op, expectedOp);
+            expect_value(
+                __wrap_synth_operatorAttackRate, attackRate, expectedValue);
 
-        __real_midi_cc(0, cc, 8);
+            __real_midi_cc(chan, cc, 8);
+        }
     }
 }
 
 static void test_midi_sets_operator_first_decay_rate(UNUSED void** state)
 {
     const u8 expectedValue = 2;
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 47; cc <= 50; cc++) {
+            u8 expectedOp = cc - 47;
+            expect_value(__wrap_synth_operatorFirstDecayRate, channel, chan);
+            expect_value(__wrap_synth_operatorFirstDecayRate, op, expectedOp);
+            expect_value(__wrap_synth_operatorFirstDecayRate, firstDecayRate,
+                expectedValue);
 
-    for (u8 cc = 47; cc <= 50; cc++) {
-        u8 expectedOp = cc - 47;
-        expect_value(__wrap_synth_operatorFirstDecayRate, channel, 0);
-        expect_value(__wrap_synth_operatorFirstDecayRate, op, expectedOp);
-        expect_value(
-            __wrap_synth_operatorFirstDecayRate, firstDecayRate, expectedValue);
-
-        __real_midi_cc(0, cc, 8);
+            __real_midi_cc(chan, cc, 8);
+        }
     }
 }
 
@@ -262,14 +288,16 @@ static void test_midi_sets_operator_second_decay_rate(UNUSED void** state)
 {
     const u8 expectedValue = 1;
 
-    for (u8 cc = 51; cc <= 54; cc++) {
-        u8 expectedOp = cc - 51;
-        expect_value(__wrap_synth_operatorSecondDecayRate, channel, 0);
-        expect_value(__wrap_synth_operatorSecondDecayRate, op, expectedOp);
-        expect_value(__wrap_synth_operatorSecondDecayRate, secondDecayRate,
-            expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 51; cc <= 54; cc++) {
+            u8 expectedOp = cc - 51;
+            expect_value(__wrap_synth_operatorSecondDecayRate, channel, chan);
+            expect_value(__wrap_synth_operatorSecondDecayRate, op, expectedOp);
+            expect_value(__wrap_synth_operatorSecondDecayRate, secondDecayRate,
+                expectedValue);
 
-        __real_midi_cc(0, cc, 8);
+            __real_midi_cc(chan, cc, 8);
+        }
     }
 }
 
@@ -277,14 +305,18 @@ static void test_midi_sets_operator_secondary_amplitude(UNUSED void** state)
 {
     const u8 expectedValue = 1;
 
-    for (u8 cc = 55; cc <= 58; cc++) {
-        u8 expectedOp = cc - 55;
-        expect_value(__wrap_synth_operatorSecondaryAmplitude, channel, 0);
-        expect_value(__wrap_synth_operatorSecondaryAmplitude, op, expectedOp);
-        expect_value(__wrap_synth_operatorSecondaryAmplitude,
-            secondaryAmplitude, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 55; cc <= 58; cc++) {
+            u8 expectedOp = cc - 55;
+            expect_value(
+                __wrap_synth_operatorSecondaryAmplitude, channel, chan);
+            expect_value(
+                __wrap_synth_operatorSecondaryAmplitude, op, expectedOp);
+            expect_value(__wrap_synth_operatorSecondaryAmplitude,
+                secondaryAmplitude, expectedValue);
 
-        __real_midi_cc(0, cc, 8);
+            __real_midi_cc(chan, cc, 8);
+        }
     }
 }
 
@@ -292,14 +324,18 @@ static void test_midi_sets_operator_amplitude_modulation(UNUSED void** state)
 {
     const u8 expectedValue = 1;
 
-    for (u8 cc = 70; cc <= 73; cc++) {
-        u8 expectedOp = cc - 70;
-        expect_value(__wrap_synth_operatorAmplitudeModulation, channel, 0);
-        expect_value(__wrap_synth_operatorAmplitudeModulation, op, expectedOp);
-        expect_value(__wrap_synth_operatorAmplitudeModulation,
-            amplitudeModulation, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 70; cc <= 73; cc++) {
+            u8 expectedOp = cc - 70;
+            expect_value(
+                __wrap_synth_operatorAmplitudeModulation, channel, chan);
+            expect_value(
+                __wrap_synth_operatorAmplitudeModulation, op, expectedOp);
+            expect_value(__wrap_synth_operatorAmplitudeModulation,
+                amplitudeModulation, expectedValue);
 
-        __real_midi_cc(0, cc, 96);
+            __real_midi_cc(chan, cc, 96);
+        }
     }
 }
 
@@ -307,14 +343,16 @@ static void test_midi_sets_operator_release_rate(UNUSED void** state)
 {
     const u8 expectedValue = 1;
 
-    for (u8 cc = 59; cc <= 62; cc++) {
-        u8 expectedOp = cc - 59;
-        expect_value(__wrap_synth_operatorReleaseRate, channel, 0);
-        expect_value(__wrap_synth_operatorReleaseRate, op, expectedOp);
-        expect_value(
-            __wrap_synth_operatorReleaseRate, releaseRate, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = 59; cc <= 62; cc++) {
+            u8 expectedOp = cc - 59;
+            expect_value(__wrap_synth_operatorReleaseRate, channel, chan);
+            expect_value(__wrap_synth_operatorReleaseRate, op, expectedOp);
+            expect_value(
+                __wrap_synth_operatorReleaseRate, releaseRate, expectedValue);
 
-        __real_midi_cc(0, cc, 8);
+            __real_midi_cc(chan, cc, 8);
+        }
     }
 }
 
@@ -325,13 +363,15 @@ static void test_midi_sets_operator_ssg_eg(UNUSED void** state)
     const u8 MIN_CC = 90;
     const u8 MAX_CC = 93;
 
-    for (u8 cc = MIN_CC; cc <= MAX_CC; cc++) {
-        u8 expectedOp = cc - MIN_CC;
-        expect_value(__wrap_synth_operatorSsgEg, channel, 0);
-        expect_value(__wrap_synth_operatorSsgEg, op, expectedOp);
-        expect_value(__wrap_synth_operatorSsgEg, ssgEg, expectedValue);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        for (u8 cc = MIN_CC; cc <= MAX_CC; cc++) {
+            u8 expectedOp = cc - MIN_CC;
+            expect_value(__wrap_synth_operatorSsgEg, channel, chan);
+            expect_value(__wrap_synth_operatorSsgEg, op, expectedOp);
+            expect_value(__wrap_synth_operatorSsgEg, ssgEg, expectedValue);
 
-        __real_midi_cc(0, cc, 88);
+            __real_midi_cc(chan, cc, 88);
+        }
     }
 }
 
@@ -340,12 +380,13 @@ static void test_midi_sets_genmdm_stereo_mode(UNUSED void** state)
     const u8 expectedMode = 3;
     const u8 value = 127;
     const u8 cc = 77;
-    const u8 channel = 0;
 
-    expect_value(__wrap_synth_stereo, channel, channel);
-    expect_value(__wrap_synth_stereo, mode, expectedMode);
+    for (u8 chan = 0; chan < MAX_FM_CHANS; chan++) {
+        expect_value(__wrap_synth_stereo, channel, chan);
+        expect_value(__wrap_synth_stereo, mode, expectedMode);
 
-    __real_midi_cc(channel, cc, value);
+        __real_midi_cc(chan, cc, value);
+    }
 }
 
 static void test_midi_sets_global_LFO_enable(UNUSED void** state)
