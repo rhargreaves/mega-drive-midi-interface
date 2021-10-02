709f4c4d479bb60c73165b5e1ae2cbeea514ee82
Robert Hargreaves <3618663+rhargreaves@users.noreply.github.com>
2021-10-02 21:07:18 +0100

    Use constants instead of magic numbers

--- src/midi_sender.c
+++ src/midi_sender.c
@@ -1,12 +1,13 @@
+#include "midi.h"
 #include "midi_sender.h"
 #include "comm.h"
 
 void midi_sender_send_sysex(const u8* data, u16 length)
 {
-    comm_write(0xF0);
+    comm_write(SYSEX_START);
     for (u16 i = 0; i < length; i++) {
 
         comm_write(data[i]);
     }
-    comm_write(0xF7);
+    comm_write(SYSEX_END);
 }
