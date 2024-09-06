59deb132cb5ef22cc7a583ea540382d0633949bc
Robert Hargreaves <kiyouta@gmail.com>
2024-09-06 22:00:48 +0100

    Formatting++

--- src/boot/rom_head.c
+++ src/boot/rom_head.c
@@ -1,19 +1,31 @@
 #include "genesis.h"
 
+// clang-format off
 __attribute__((externally_visible)) const ROMHeader rom_header = {
 #if MEGAWIFI
     "SEGA MEGAWIFI   ",
 #else
     "SEGA SSF        ",
 #endif
-    "(C)ROB H. 2024  ", "MEGA DRIVE MIDI INTERFACE                       ",
-    "MEGA DRIVE MIDI INTERFACE                       ", "GM 00000000-00", 0x000, "JD              ",
+    "(C)ROB H. 2024  ",
+    "MEGA DRIVE MIDI INTERFACE                       ",
+    "MEGA DRIVE MIDI INTERFACE                       ",
+    "GM 00000000-00",
+    0x000,
+    "JD              ",
     0x00000000,
 #if (ENABLE_BANK_SWITCH != 0)
     0x003FFFFF,
 #else
     0x000FFFFF,
 #endif
-    0xE0FF0000, 0xE0FFFFFF, "RA", 0xF820, 0x00200000, 0x0020FFFF, "            ",
-    "                                        ", "JUE             "
+    0xE0FF0000,
+    0xE0FFFFFF,
+    "RA",
+    0xF820,
+    0x00200000,
+    0x0020FFFF,
+    "            ",
+    "                                        ",
+    "JUE             "
 };
