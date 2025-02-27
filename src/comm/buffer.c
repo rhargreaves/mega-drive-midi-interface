6d5a0a18740b783feea82542765abb35b4138f88
Robert Hargreaves <kiyouta@gmail.com>
2025-02-27 23:35:54 +0000

    Buffer: Use conventional names for read/write pointers

--- src/comm/buffer.c
+++ src/comm/buffer.c
@@ -1,13 +1,13 @@
 #include "buffer.h"
 
-static volatile u16 readHead = 0;
-static volatile u16 writeHead = 0;
+static volatile u16 tail = 0;
+static volatile u16 head = 0;
 static volatile u8 buffer[BUFFER_SIZE];
 
 void buffer_init(void)
 {
-    readHead = 0;
-    writeHead = 0;
+    tail = 0;
+    head = 0;
 }
 
 buffer_status_t buffer_read(u8* data)
@@ -16,45 +16,45 @@ buffer_status_t buffer_read(u8* data)
         return BUFFER_ERROR;
     }
 
-    if (readHead == writeHead) {
+    if (tail == head) {
         return BUFFER_EMPTY;
     }
 
-    *data = buffer[readHead];
-    readHead = (readHead + 1) % BUFFER_SIZE;
+    *data = buffer[tail];
+    tail = (tail + 1) % BUFFER_SIZE;
     return BUFFER_OK;
 }
 
 buffer_status_t buffer_write(u8 data)
 {
-    u16 nextWriteHead = (writeHead + 1) % BUFFER_SIZE;
-    if (nextWriteHead == readHead) {
+    u16 nextHead = (head + 1) % BUFFER_SIZE;
+    if (nextHead == tail) {
         return BUFFER_FULL;
     }
 
-    buffer[writeHead] = data;
-    writeHead = nextWriteHead;
+    buffer[head] = data;
+    head = nextHead;
     return BUFFER_OK;
 }
 
 bool buffer_can_read(void)
 {
-    return readHead != writeHead;
+    return tail != head;
 }
 
 u16 buffer_available(void)
 {
-    if (readHead == writeHead) {
+    if (tail == head) {
         return BUFFER_CAPACITY;
-    } else if (readHead < writeHead) {
-        return BUFFER_CAPACITY - (writeHead - readHead);
+    } else if (tail < head) {
+        return BUFFER_CAPACITY - (head - tail);
     } else {
-        return readHead - writeHead - 1;
+        return tail - head - 1;
     }
 }
 
 bool buffer_can_write(void)
 {
-    u16 nextWriteHead = (writeHead + 1) % BUFFER_SIZE;
-    return nextWriteHead != readHead;
+    u16 nextHead = (head + 1) % BUFFER_SIZE;
+    return nextHead != tail;
 }
