020e839f0af7d80bcc121acae9b4fe6c2fe850d3
Robert Hargreaves <kiyouta@gmail.com>
2025-05-24 10:15:30 +0100

    refactor: remove midi_cc module

--- src/midi_cc.c
+++
@@ -1,7 +0,0 @@
-#include "midi_cc.h"
-#include "midi.h"
-
-void midi_cc_raw(u8 chan, u8 controller, u8 value)
-{
-    midi_cc(chan, controller, value);
-}
--- src/midi_cc.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "genesis.h"
-
-void midi_cc_raw(u8 chan, u8 controller, u8 value);
