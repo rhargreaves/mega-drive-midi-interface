eb774091776f990e9698254da72a79c4e852c0c2
Robert Hargreaves <kiyouta@gmail.com>
2025-05-25 22:30:47 +0100

    refactor: tidy up pitchcents module

--- src/pitchcents.c
+++ src/pitchcents.c
@@ -5,16 +5,11 @@ PitchCents pc_bend(u8 pitch, s8 cents, u16 pitchBend, PitchCents range)
 {
     s16 centsRange = range.pitch * 100 + range.cents;
     s16 centsAdd = ((pitchBend - MIDI_PITCH_BEND_CENTRE) * centsRange) / 8192;
-    PitchCents pc = { .pitch = pitch, .cents = cents };
-    return pc_shift(pc, centsAdd);
+    return pc_shift((PitchCents) { .pitch = pitch, .cents = cents }, centsAdd);
 }
 
 PitchCents pc_shift(PitchCents pc, s16 centsAdd)
 {
-    u16 totalCents = (pc.pitch * 100) + pc.cents;
-    totalCents += centsAdd;
-
-    pc.pitch = totalCents / 100;
-    pc.cents = totalCents % 100;
-    return pc;
+    u16 totalCents = ((pc.pitch * 100) + pc.cents) + centsAdd;
+    return (PitchCents) { .pitch = totalCents / 100, .cents = totalCents % 100 };
 }
