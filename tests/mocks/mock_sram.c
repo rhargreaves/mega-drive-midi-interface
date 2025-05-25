c288bfd2bf82e002c0d36f9aa112cd057da3859f
Robert Hargreaves <kiyouta@gmail.com>
2025-05-25 19:04:43 +0100

    fix: wrap signature for SRAM_readByte

--- tests/mocks/mock_sram.c
+++ tests/mocks/mock_sram.c
@@ -19,18 +19,18 @@ void __wrap_SRAM_writeByte(u32 offset, u8 data)
     sram_data[offset] = data;
 }
 
-u8* __wrap_SRAM_readByte(u32 offset)
+u8 __wrap_SRAM_readByte(u32 offset)
 {
     if (!enabled) {
         print_error("SRAM read attempted when disabled");
-        return NULL;
+        return 0;
     }
 
     if (offset >= sizeof(sram_data)) {
         print_error("SRAM read out of bounds: %ld", offset);
-        return NULL;
+        return 0;
     }
-    return &sram_data[offset];
+    return sram_data[offset];
 }
 
 u8* mock_sram_data(u32 offset)
--- tests/mocks/mock_sram.h
+++ tests/mocks/mock_sram.h
@@ -2,7 +2,7 @@
 #include "genesis.h"
 
 void __wrap_SRAM_writeByte(u32 offset, u8 data);
-u8* __wrap_SRAM_readByte(u32 offset);
+u8 __wrap_SRAM_readByte(u32 offset);
 void __wrap_SRAM_enable(void);
 void __wrap_SRAM_disable(void);
 
