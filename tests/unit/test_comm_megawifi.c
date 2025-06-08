1229e228c5c67a26a4e487caff0cc57cd4c87608
Robert Hargreaves <kiyouta@gmail.com>
2025-06-08 19:58:53 +0100

    fix: tests relating to MW init

--- tests/unit/test_comm_megawifi.c
+++ tests/unit/test_comm_megawifi.c
@@ -55,13 +55,10 @@ static void expect_ap_connection(void)
     will_return(__wrap_mw_ap_assoc_wait, MW_ERR_NONE);
 }
 
-static void expect_ip_log(void)
+static void expect_ip_cfg(void)
 {
     mock_ip_cfg(IP(127, 1, 2, 3));
     will_return(__wrap_mw_ip_current, MW_ERR_NONE);
-    if (settings_debug_megawifi_init()) {
-        expect_log_info("MW: IP: %s");
-    }
 }
 
 static void megawifi_init(void)
@@ -69,17 +66,19 @@ static void megawifi_init(void)
     expect_any(__wrap_scheduler_addTickHandler, onTick);
     expect_any(__wrap_scheduler_addFrameHandler, onFrame);
     will_return(__wrap_mw_uart_is_present, true);
+    expect_log_info("MW: Detecting...");
+    expect_scheduler_yield();
     expect_mw_init();
+    expect_log_info("MW: Found v%d.%d");
+    expect_scheduler_yield();
     expect_mw_detect();
+    expect_log_info("MW: Ctrl UDP %s:%u");
+    expect_scheduler_yield();
     expect_scheduler_yield();
     expect_ap_connection();
-    expect_ip_log();
-    expect_scheduler_yield();
+    expect_ip_cfg();
     expect_udp_port_open(CH_CONTROL_PORT, "5006");
     expect_udp_port_open(CH_MIDI_PORT, "5007");
-    if (settings_debug_megawifi_init()) {
-        expect_log_info("MW: Listening on UDP %d");
-    }
     __real_comm_megawifi_init();
 }
 
